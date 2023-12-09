bb4dc9103a3a540a77a43b71d93d733199034645
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2023-12-09 15:36:18 -0300

    Day 9

---
+++ 2023/09-mirage-maintenance-part2.py
@@ -0,0 +1,17 @@
+input_data = open('./09-input.txt', 'r').readlines()
+# input_data = open('./fake-input.txt', 'r').readlines()
+
+
+def get_next(numbers) -> int:
+    new_numbers = [numbers[i + 1] - numbers[i] for i in range(len(numbers) - 1)]
+    return new_numbers[0] if len(set(new_numbers)) == 1 else new_numbers[0] - get_next(new_numbers)
+
+
+new_items = []
+for line in input_data:
+    numbers = list(map(int, line.split()))
+    new_items.append(numbers[0] - get_next(numbers))
+    # print(new_items[-1], new_items[-1] + numbers[0])
+
+print(sum(new_items))
+# 1066
